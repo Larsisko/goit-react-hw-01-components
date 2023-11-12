30c723630a21ba78888c8b5b28f2c400f7a2ba8d
Larsisko <arkadiusz.miazga1988@gmail.com>
2023-11-12 17:56:53 +0100

    all task done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,10 @@ import Profile from './Profile.js/Profile';
 import user from 'user.json';
 import Statistic from './Statistic.js/Statistic';
 import data from 'data.json';
+import friends from 'friends.json';
+import FriendList from './FrendListJS/FrendList';
+import transactions from 'transactions.json';
+import TransactionHistory from './transactionHistory/transactionHistory';
 
 export const App = () => {
   return (
@@ -15,6 +19,8 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistic title="Upload stats" stats={data} />
+      <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
---
+++ src/components/FrendListJS/FrendList.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from '../Css/friendList.module/friendList.module.css';
+
+const FriendListItem = ({ avatar, name, isOnline }) => (
+  <li className={styles.item}>
+    <span className={isOnline ? styles.online : styles.offline}></span>
+    <img
+      src={avatar}
+      alt={`${name}'s avatar`}
+      className={styles.avatar}
+      width="48"
+    />
+    <p className={styles.name}>{name}</p>
+  </li>
+);
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
+
+const FriendList = ({ friends }) => (
+  <ul className={styles['friend-list']}>
+    {friends.map(({ id, avatar, name, isOnline }) => (
+      <FriendListItem
+        key={id}
+        avatar={avatar}
+        name={name}
+        isOnline={isOnline}
+      />
+    ))}
+  </ul>
+);
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
+};
+
+export default FriendList;
